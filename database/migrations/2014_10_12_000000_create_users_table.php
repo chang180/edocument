da1bd2fd1f3aea94144510120953fa143c47fcc6
chang180 <chang180@gmail.com>
2023-01-18 14:49:23 +0800

    完成資料庫設定，並將users表格改為目前格式

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,20 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('staff_id')->nullable();
+            $table->string('group')->nullable();
+            $table->string('name')->nullable();
+            $table->string('user_name')->nullable();
+            $table->string('eng_name')->nullable();
             $table->string('password');
+            $table->string('email')->unique();
+            $table->string('dpt')->nullable();
+            $table->string('jobtitle')->nullable();
+            $table->string('auth')->nullable();
+            $table->string('agent_staff_id')->nullable();
+            $table->string('agent_group')->nullable();
+            $table->string('is_enabled')->default('1');
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
